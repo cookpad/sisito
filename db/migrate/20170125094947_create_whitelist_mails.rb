d788a1e88e07439d8578fed22f9714e3cab67ee9
Genki Sugawara <sugawara@cookpad.com>
2017-01-27 12:15:27 +0900

    Fix schema

--- db/migrate/20170125094947_create_whitelist_mails.rb
+++ db/migrate/20170125094947_create_whitelist_mails.rb
@@ -1,10 +1,10 @@
 class CreateWhitelistMails < ActiveRecord::Migration[5.0]
   def change
     create_table "whitelist_mails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-      t.string   "recipient"
+      t.string   "recipient",    default: "", null: false
       t.string   "senderdomain", default: "", null: false
-      t.datetime "created_at",               null: false
-      t.datetime "updated_at",               null: false
+      t.datetime "created_at",                null: false
+      t.datetime "updated_at",                null: false
       t.index ["recipient", "senderdomain"], name: "idx_recipient_senderdomain", unique: true, using: :btree
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170125094947) do
   end
 
   create_table "whitelist_mails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "recipient"
+    t.string   "recipient",    default: "", null: false
     t.string   "senderdomain", default: "", null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
