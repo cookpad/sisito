46b0e10ab6ca2af4b9228be5c7d2a2fb876c8207
Genki Sugawara <sugawara@cookpad.com>
2017-01-27 11:31:09 +0900

    Fix schema

--- db/migrate/20170125094947_create_whitelist_mails.rb
+++ db/migrate/20170125094947_create_whitelist_mails.rb
@@ -1,10 +1,11 @@
 class CreateWhitelistMails < ActiveRecord::Migration[5.0]
   def change
-    create_table "whitelist_mails" do |t|
+    create_table "whitelist_mails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
       t.string   "recipient"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["recipient"], name: "idx_recipient", unique: true, using: :btree
+      t.string   "destination", default: "", null: false
+      t.datetime "created_at",               null: false
+      t.datetime "updated_at",               null: false
+      t.index ["recipient", "destination"], name: "idx_recipient_destination", unique: true, using: :btree
     end
 
     add_foreign_key "whitelist_mails", "bounce_mails", column: "recipient", primary_key: "recipient", name: "whitelist_mails_ibfk_1", on_update: :cascade, on_delete: :cascade
