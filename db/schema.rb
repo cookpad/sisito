9db75d734b126713a483ed3b8debba410ef3636b
Genki Sugawara <sugawara@cookpad.com>
2017-01-29 00:01:36 +0900

    Add example data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125094947) do
+ActiveRecord::Schema.define(version: 20170128144003) do
 
   create_table "bounce_mails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "timestamp",                   null: false
