23f7b77dd109c7c2b7baff28285761b53bcd892f
Genki Sugawara <sugawara@cookpad.com>
2017-02-01 22:55:56 +0900

    Fix etc..

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Sisito
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Tokyo'
   end
 end
