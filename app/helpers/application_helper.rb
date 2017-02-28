4fd628950fbac0cd0e82dfe289f81c4ebfcdca1f
Genki Sugawara <sugawara@cookpad.com>
2017-02-28 13:43:32 +0900

    Add mail link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def mail_link(bounce_mail, options = {target: '_blank'})
+    if bounce_mail.link
+      link_to(bounce_mail.recipient, bounce_mail.link, options)
+    else
+      bounce_mail.recipient
+    end
+  end
 end
--- app/models/bounce_mail.rb
+++ app/models/bounce_mail.rb
@@ -4,4 +4,14 @@ class BounceMail < ApplicationRecord
     user.gsub!(/./, '*')
     [user, domain].join(?@)
   end
+
+  def link
+    mail_link = Rails.application.config.sisito[:mail_link]
+
+    if mail_link
+      mail_link % {digest: self.digest}
+    else
+      nil
+    end
+  end
 end
