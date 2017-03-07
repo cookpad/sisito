419b1df7cf12567f5236acd95c6046eb63025003
Genki Sugawara <sugawara@cookpad.com>
2017-03-08 01:37:22 +0900

    small fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, if: :authenticate?
+
+  if Rails.application.config.sisito.dig(:omniauth, :google_client_id)
+    before_action :authenticate_user!, if: :authenticate?
+  end
 
   private
 
   def authenticate_user!
-    if Rails.application.config.sisito.dig(:omniauth, :google_client_id) and session[:auth].blank?
+    if session[:auth].blank?
       redirect_to '/auth/google_oauth2'
     end
   end
