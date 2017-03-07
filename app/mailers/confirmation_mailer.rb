c373ed7583c4a01886bdd3041c5632632c2218b2
Genki Sugawara <sugawara@cookpad.com>
2017-03-07 20:09:38 +0900

    Small fix

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,7 +1,6 @@
 class ConfirmationMailer < ApplicationMailer
   def email(confirmation_mail)
     delivery_options = Rails.application.config.sisito.fetch(:smtp).fetch(confirmation_mail.from)
-    delivery_options.symbolize_keys!
 
     mail(from: confirmation_mail.from,
          to: confirmation_mail.to,
