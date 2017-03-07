9cb5b5b985c9d09a3a94218c969b41e80412bccc
Genki Sugawara <sugawara@cookpad.com>
2017-03-07 20:16:58 +0900

    Small fix

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,6 +1,7 @@
 class ConfirmationMailer < ApplicationMailer
   def email(confirmation_mail)
     delivery_options = Rails.application.config.sisito.fetch(:smtp).fetch(confirmation_mail.from)
+    delivery_options = delivery_options.to_h.symbolize_keys
 
     mail(from: confirmation_mail.from,
          to: confirmation_mail.to,
--- config/initializers/sisito.rb
+++ config/initializers/sisito.rb
@@ -2,5 +2,4 @@ Rails.application.config.tap do |config|
   yaml = YAML.load_file(Rails.root.join('config/sisito.yml'))
   config.sisito = yaml.with_indifferent_access
   config.sisito[:digest] = Digest.const_get(config.sisito.fetch(:digest).upcase)
-  config.sisito.fetch(:smtp, {}).values.each(&:symbolize_keys!)
 end
