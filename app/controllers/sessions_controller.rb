9a34844f65c7e8a783f5e69278d071e5fa1ca98b
Genki Sugawara <sugawara@cookpad.com>
2017-03-08 15:46:59 +0900

    Add allow_users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
   def callback
     auth = request.env['omniauth.auth']
-    session[:auth] = auth.info
-    redirect_to root_path
+    allow_users = Rails.application.config.sisito.dig(:omniauth, :allow_users)
+
+    if allow_users.blank? or allow_users.include?(auth.info.email)
+      session[:auth] = auth.info
+      redirect_to root_path
+    else
+      render plain: '401 Unauthorized', status: :unauthorized
+    end
   end
 
   def failure
