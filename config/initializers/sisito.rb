a421ba65065aa9b8b3e4222acb4f3ba77ccf33c4
Genki Sugawara <sugawara@cookpad.com>
2017-05-25 13:42:30 +0900

    Add admin/secret_file

--- config/initializers/sisito.rb
+++ config/initializers/sisito.rb
@@ -2,4 +2,11 @@ Rails.application.config.tap do |config|
   yaml = YAML.load_file(Rails.root.join('config/sisito.yml'))
   config.sisito = yaml.with_indifferent_access
   config.sisito[:digest] = Digest.const_get(config.sisito.fetch(:digest).upcase)
+
+  if (secret_file = config.sisito.fetch(:admin, {})[:secret_file])
+    secret_file = Rails.root.join(secret_file) unless secret_file.start_with?('/')
+    secret = YAML.load_file(secret_file)
+    config.sisito[:admin][:username] = secret.fetch('username')
+    config.sisito[:admin][:password] = secret.fetch('password')
+  end
 end
